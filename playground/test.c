30b7138b7576d21f8feaabaaa534819a91c6e10d
TimIlTom <ittomalupu@gmail.com>
2024-11-03 16:36:49 +0100

    playground and .vscode folders

---
+++ playground/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int userSize = 5;
+
+    int middle = userSize - 1;
+
+    for(){
+        for(){
+
+            printf();
+        }
+    }
+
+    return 0;
+}
